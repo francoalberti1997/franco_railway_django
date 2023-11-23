6f4545db4162347af752a4cd6d78af8d42d5d9eb
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2023-11-23 14:48:25 -0300

    3rd commit

--- franco_railway/settings.py
+++ franco_railway/settings.py
@@ -127,4 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
